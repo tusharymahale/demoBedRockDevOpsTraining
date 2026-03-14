fdeb33e0e7262c31be256d394db4e43458ada292
Tushar <tusharym@gmail.com>
2026-03-14 10:32:24 +0000

    commit to trigger pipeline and sonarqube added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer { // 1. Extend this
 
-	public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class); // 2. Override this method
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
+
